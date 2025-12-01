490bdbbf535e02f2808384da56b151a509fb01fb
IldusGaniev <74356807+IldusGaniev@users.noreply.github.com>
2025-12-01 13:05:22 +0500

    Delete linkitsum-bff directory

--- linkitsum-bff/src/main/java/ru/ildus/linkitsumbff/dto/NewGroupPaylod.java
+++
@@ -1,8 +0,0 @@
-package ru.ildus.linkitsumbff.dto;
-
-public record NewGroupPaylod(
-        String name,
-        String color,
-        String user_id
-) {
-}
--- linkitsum-bff/src/main/java/ru/ildus/linkitsumbff/dto/UpdateGroupPaylod.java
+++
@@ -1,9 +0,0 @@
-package ru.ildus.linkitsumbff.dto;
-
-public record UpdateGroupPaylod(
-        long id,
-        String name,
-        String color,
-        String user_id
-) {
-}
